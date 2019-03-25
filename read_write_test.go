33bd598be73047cd0a8488ee80e4460d32bf4697
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-03-24 18:23:56 -0700

    finish first round of siser

--- read_write_test.go
+++ read_write_test.go
@@ -5,8 +5,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"math/rand"
-	"os"
 	"strconv"
 	"testing"
 	"time"
@@ -30,37 +28,6 @@ func genLargeValue() {
 	largeValue = s
 }
 
-func genSerializedSiser() {
-	var rec Record
-	rec.Append("uri", "/atom.xml")
-	rec.Append("code", strconv.Itoa(200))
-	rec.Append("ip", "54.186.248.49")
-	durMs := float64(1.41) / float64(time.Millisecond)
-	durStr := strconv.FormatFloat(durMs, 'f', 2, 64)
-	rec.Append("dur", durStr)
-	rec.Append("when", time.Now().Format(time.RFC3339))
-	rec.Append("size", strconv.Itoa(35286))
-	rec.Append("ua", "Feedspot http://www.feedspot.com")
-	rec.Append("referer", "http://blog.kowalczyk.info/feed")
-	serializedSiser = rec.Marshal()
-}
-
-func genSerializedJSON() {
-	rec := testRecJSON{
-		URI:       "/atom.xml",
-		Code:      200,
-		IP:        "54.186.248.49",
-		Duration:  time.Microsecond * time.Duration(1410),
-		When:      time.Now(),
-		Size:      35286,
-		UserAgent: "Feedspot http://www.feedspot.com",
-		Referer:   "http://blog.kowalczyk.info/feed",
-	}
-	d, err := json.Marshal(rec)
-	panicIfErr(err)
-	serializedJSON = d
-}
-
 func init() {
 	genLargeValue()
 	genSerializedSiser()
@@ -68,29 +35,18 @@ func init() {
 }
 
 func bufReaderFromBytes(d []byte) *bufio.Reader {
-	r := bytes.NewBuffer(d)
-	return bufio.NewReader(r)
+	return bufio.NewReader(bytes.NewBuffer(d))
 }
 
 func testRoundTrip(t *testing.T, recIn *Record) string {
 	d := recIn.Marshal()
-	br := bufReaderFromBytes(d)
-	rec := Record{}
-	_, err := ReadRecord(br, &rec)
+	rec, err := UnmarshalRecord(d, nil)
 	assert.NoError(t, err)
-	assert.Equal(t, rec.Keys, recIn.Keys)
-	assert.Equal(t, rec.Values, recIn.Values)
+	assert.Equal(t, rec.Entries, recIn.Entries)
 	return string(d)
 }
 
-func TestWriterSizePrefixed(t *testing.T) {
-	testWriter(t, FormatSizePrefix)
-}
-
-func TestWriterSeparator(t *testing.T) {
-	testWriter(t, FormatSeparator)
-}
-
+/*
 func testWriter(t *testing.T, format Format) {
 	strings := []string{"hey\n", "ho"}
 	names := []string{"", "with name"}
@@ -103,13 +59,11 @@ ho
 	buf := &bytes.Buffer{}
 	w := NewWriter(buf)
 	w.Format = format
+	unixNano := 5 * time.Second
+	tm := time.Unix(0, int64(unixNano))
 	for i, s := range strings {
 		name := names[i]
-		if name == "" {
-			_, err = w.Write([]byte(s))
-		} else {
-			_, err = w.WriteNamed([]byte(s), name)
-		}
+		_, err = w.WriteNamed([]byte(s), tm, name)
 		assert.NoError(t, err)
 	}
 	s := buf.String()
@@ -126,42 +80,32 @@ ho
 	}
 	assert.NoError(t, r.Err())
 }
+*/
 
 func TestRecordSerializeSimple(t *testing.T) {
 	var r Record
 	r.Append("key", "val")
 	s := testRoundTrip(t, &r)
-	assert.Equal(t, "key: val\n---\n", s)
+	assert.Equal(t, "key: val\n", s)
 }
 
 func TestRecordSerializeSimple2(t *testing.T) {
 	var r Record
 	r.Append("k2", "a\nb")
 	s := testRoundTrip(t, &r)
-	assert.Equal(t, "k2:+3\na\nb\n---\n", s)
+	assert.Equal(t, "k2:+3\na\nb\n", s)
 }
 
 func TestRecordSerializeSimple3(t *testing.T) {
 	var r Record
 	r.Append("long key", largeValue)
 	got := testRoundTrip(t, &r)
-	exp := fmt.Sprintf("long key:+%d\n%s\n---\n", len(largeValue), largeValue)
+	exp := fmt.Sprintf("long key:+%d\n%s\n", len(largeValue), largeValue)
 	assert.Equal(t, exp, got)
 }
 
-func TestManySeparator(t *testing.T) {
-	testMany(t, FormatSeparator, "")
-}
-
-func TestManySizePrefixed(t *testing.T) {
-	testMany(t, FormatSizePrefix, "")
-}
-
-func TestManySizePrefixedNamed(t *testing.T) {
-	testMany(t, FormatSizePrefix, "named")
-}
-
-func testMany(t *testing.T, format Format, name string) {
+/*
+func testMany(t *testing.T, name string) {
 	path := "test.txt"
 	os.Remove(path)
 	f, err := os.Create(path)
@@ -172,7 +116,6 @@ func testMany(t *testing.T, format Format, name string) {
 	defer os.Remove(path)
 
 	w := NewWriter(f)
-	w.Format = format
 
 	rec := &Record{}
 	var positions []int64
@@ -222,6 +165,7 @@ func testMany(t *testing.T, format Format, name string) {
 	assert.NoError(t, reader.Err())
 	assert.Equal(t, nRecs, i)
 }
+*/
 
 func TestAppendPanics(t *testing.T) {
 	rec := &Record{}
@@ -229,31 +173,19 @@ func TestAppendPanics(t *testing.T) {
 }
 
 func TestIntStrLen(t *testing.T) {
-	tests := []struct {
-		n   int
-		exp int
-	}{
-		{-1, 2},
-		{-89, 3},
-		{-319, 4},
-		{-2341, 5},
-		{0, 1},
-		{9, 1},
-		{10, 2},
-		{34, 2},
-		{99, 2},
-		{100, 3},
-		{823, 3},
-		{999, 3},
-		{1000, 4},
-		{3241, 4},
-		{9999, 4},
-		{10000, 5},
-		{99999, 5},
+	numbers := []int{-1, 0, 1}
+	n1 := 1
+	n2 := -1
+	for i := 0; i < 10; i++ {
+		n1 := n1*10 + i + 1
+		numbers = append(numbers, n1)
+		n2 := n2*10 - i - 1
+		numbers = append(numbers, n2)
 	}
-	for _, test := range tests {
-		got := intStrLen(test.n)
-		assert.Equal(t, test.exp, got)
+	for _, n := range numbers {
+		got := intStrLen(n)
+		exp := len(strconv.Itoa(n))
+		assert.Equal(t, exp, got)
 	}
 }
 
@@ -324,16 +256,42 @@ func BenchmarkJSONMarshal(b *testing.B) {
 	}
 }
 
-var (
-	lines []string
-)
+func genSerializedSiser() {
+	var rec Record
+	rec.Append("uri", "/atom.xml")
+	rec.Append("code", strconv.Itoa(200))
+	rec.Append("ip", "54.186.248.49")
+	durMs := float64(1.41) / float64(time.Millisecond)
+	durStr := strconv.FormatFloat(durMs, 'f', 2, 64)
+	rec.Append("dur", durStr)
+	rec.Append("when", time.Now().Format(time.RFC3339))
+	rec.Append("size", strconv.Itoa(35286))
+	rec.Append("ua", "Feedspot http://www.feedspot.com")
+	rec.Append("referer", "http://blog.kowalczyk.info/feed")
+	serializedSiser = rec.Marshal()
+}
+
+func genSerializedJSON() {
+	rec := testRecJSON{
+		URI:       "/atom.xml",
+		Code:      200,
+		IP:        "54.186.248.49",
+		Duration:  time.Microsecond * time.Duration(1410),
+		When:      time.Now(),
+		Size:      35286,
+		UserAgent: "Feedspot http://www.feedspot.com",
+		Referer:   "http://blog.kowalczyk.info/feed",
+	}
+	d, err := json.Marshal(rec)
+	panicIfErr(err)
+	serializedJSON = d
+}
 
 func BenchmarkSiserUnmarshal(b *testing.B) {
 	var rec Record
 	var err error
 	for n := 0; n < b.N; n++ {
-		r := bufio.NewReader(bytes.NewBuffer(serializedSiser))
-		_, err = ReadRecord(r, &rec)
+		err = rec.Unmarshal(serializedSiser)
 		panicIfErr(err)
 	}
 }
