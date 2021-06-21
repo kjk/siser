f8578550750cbff653934055ca800d6396132239
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-06-21 15:57:37 -0700

    update how we run staticcheck on CI; remove fuzzit fuzzing

--- s/ci_static_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -xe
-
-wget -O staticcheck.tar.gz https://github.com/dominikh/go-tools/releases/download/2019.2.2/staticcheck_linux_amd64.tar.gz
-tar --strip-components 1 -C . -xf staticcheck.tar.gz staticcheck/staticcheck
-chmod ug+x ./staticcheck
-./staticcheck ./...
